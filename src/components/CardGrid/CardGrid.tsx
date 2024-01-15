c9a951d5031d46276213b23cc629498f19d4e3f9
Derek Bachmann <derek.bachmann@directsupply.com>
2024-01-15 07:09:34 -0800

    Nicks coding challenge commit

--- src/components/CardGrid/CardGrid.tsx
+++ src/components/CardGrid/CardGrid.tsx
@@ -5,29 +5,48 @@ import { Card } from "./Card";
 export const CardGrid: FC<CardGridProps> = (props) => {
 
   const cards =
-    items &&
-    items.map((item) => {
+    props.items &&
+    props.items.map((item) => {
   
       return (
         <div className={styles.gridItem}>
           <Card
-            id={id}
-            title={title}
-            price={price}
-            description={description}
-            category={category}
-            image={image}
-            rating={rating}
+            id={item.id}
+            title={item.title}
+            price={item.price}
+            description={item.description}
+            category={item.category}
+            image={item.image}
+            rating={item.rating}
           />
         </div>
       );
     });
 
   return (
-    <div>
-      <h1 className={styles.headline}>{headline}</h1>
-      <p className={styles.subHeadline}>{subheadline}</p>
+    <div className={styles.element}>
+      <h1 className={styles.headline}>{props.headline}</h1>
+      <p className={styles.subHeadline}>{props.subheadline}</p>
       <div className={styles.grid}>{cards}</div>
     </div>
   );
 };
+
+type CardGridProps = {
+  headline: string,
+  subheadline: string
+  items: CardProps[]
+}
+
+export type CardProps = {
+  id: string,
+  title: string,
+  price: number,
+  description: string,
+  category: string,
+  image: string,
+  rating: {
+    rate: number,
+    count: number
+  }
+}
