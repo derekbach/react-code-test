3b77c9228e6ffcf147de942502fdfea997453dab
Derek Bachmann <derek.bachmann@directsupply.com>
2024-01-18 08:15:18 -0800

    Introduce bugs

--- src/components/CardGrid/CardGrid.tsx
+++ src/components/CardGrid/CardGrid.tsx
@@ -2,36 +2,15 @@ import { FC } from "react";
 import styles from "./CardGrid.module.css";
 import { Card } from "./Card";
 
-export interface CardGridProps {
-  headline?: string;
-  subheadline?: string;
-  items: CardProps[] | null;
-}
-
-export interface CardProps {
-  id: number;
-  title: string;
-  price: number;
-  description: string;
-  category: string;
-  image: string;
-  rating: Rating;
-}
-
-export interface Rating {
-  rate: number;
-  count: number;
-}
 
 export const CardGrid: FC<CardGridProps> = (props) => {
-  const { headline, subheadline, items } = props;
 
   const cards =
     items &&
     items.map((item) => {
-      const { id, title, price, description, category, image, rating } = item;
+      
       return (
-        <div key={id} className={styles.gridItem}>
+        <div className={styles.gridItem}>
           <Card
             id={id}
             title={title}
